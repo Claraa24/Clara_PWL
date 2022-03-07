36900695c47bf6804afb647a2ec9f7efc05c1db1
Claraa24 <clara.adrianaorc@gmail.com>
2022-03-07 14:40:46 +0700

    Tugas pwl

---
+++ resources/views/about.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="Biodata"/>
+        <meta name="Keywords" content="Biodata"/>
+        <meta charset="UTF-8"/>
+        <title>About Me</title>
+        <link rel="stylesheet" href="{{asset('asset/css/style.css')}}" >
+        <!--<link rel="stylesheet" href="<"/>-->
+    </head>
+    <body>
+    <h1>ABOUT ME</h1>
+
+        <form action="#" style="width: 1000px"class="posisi";>
+        <fieldset class="h">
+        <table style="width: 980px;">
+            <tr>
+                <td rowspan="15" width="0px">
+                <img src="{{asset('asset/img/contact-bg.jpg')}}"  width="250px" height="420px">
+            </tr>
+            <tr>
+                <td><b>Nama Lengkap</b></td>
+                <td>:</td>
+                <td>Clara Adriana Sidauruk</td>
+            </tr>
+            <tr>
+                <td><b>NIM</b></td>
+                <td>:</td>
+                <td>2041720057</td>
+            </tr>
+            <tr>
+                <td><b>Nama Panggilan</b></td>
+                <td>:</td>
+                <td>Lala</td>
+            </tr>
+            <tr>
+                <td><b>Tempat, Tanggal Lahir</b></td>
+                <td>:</td>
+                <td>Perdagangan, 24 Januari 2002</td>
+            </tr>
+            <tr>
+                <td><b>Umur</b></td>
+                <td>:</td>
+                <td>20 Tahun</td>
+            </tr>
+            <tr>
+                <td><b>Jenis Kelamin</b></td>
+                <td>:</td>
+                <td>Perempuan</td>
+            </tr>
+            <tr>
+                <td><b>Gol. Darah</b></td>
+                <td>:</td>
+                <td>B</td>
+            </tr>
+            <tr>
+                <td><b>Agama</b></td>
+                <td>:</td>
+                <td>Kristen</td>
+            </tr>
+            <tr>
+                <td><b>Alamat</b></td>
+                <td>:</td>
+                <td>Jln.Semanggi Barat no 5e , Malang</td>
+            </tr>
+            <tr>
+                <td><b>Pekerjaan</b></td>
+                <td>:</td>
+                <td>Mahasiswa</td>
+            </tr>
+            <tr>
+                <td><b>Kewarganegaraan</b></td>
+                <td>:</td>
+                <td>Indonesia</td>
+            </tr>
+        </table>
+        </fieldset>
+       
+        </form>
+        </form>
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your Love - ClaraAdrianaa</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" href="{{asset('asset/css/style.css')}}" >
+</head>
+<body>
+
+ <!-- Header Start -->
+ <header class="header">
+ 	<div class="container">
+ 		<div class="row justify-content-between align-items-center">
+ 			<div class="brand-name">
+ 				<a href="">Je'taime</a>
+ 			</div>
+      <div class="nav-toggle">
+        <span></span>
+      </div>
+ 			<nav class="nav">
+ 				<ul>
+ 					<li><a href="#home">Home</a></li>
+ 					<li><a href="{{url('about')}}">About Me</a></li>
+ 				</ul>
+ 			</nav>
+ 		</div>
+ 	</div>
+ </header>
+ <!-- Header End -->
+
+ <!-- Home Section Start -->
+ <section class="home-section" id="home">
+ 	<!-- shape -->
+ 	<div class="shape-01">
+ 	</div>
+ 	<div class="container">
+ 		<div class="row align-items-center">
+ 			<div class="home-content">
+ 				<h1>THIS IS ME</h1>
+ 			</div>
+ 		</div>
+ 	</div>
+
+</body>
+</html>
